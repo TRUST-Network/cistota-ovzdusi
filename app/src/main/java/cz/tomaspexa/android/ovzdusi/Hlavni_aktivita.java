c0f2c43299e0a97907980d53b2cfd45a01e120fa
Tomas Pexa <tomas.pexa@trustnetwork.cz>
2017-09-10 11:53:35 +0200

    add

--- app/src/main/java/cz/tomaspexa/android/ovzdusi/Hlavni_aktivita.java
+++ app/src/main/java/cz/tomaspexa/android/ovzdusi/Hlavni_aktivita.java
@@ -1,54 +1,115 @@
 package cz.tomaspexa.android.ovzdusi;
 
-import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.params.BasicHttpParams;
+        import java.io.BufferedReader;
+        import java.io.IOException;
+        import java.io.InputStream;
+        import java.io.InputStreamReader;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+        import org.apache.http.HttpEntity;
+        import org.apache.http.HttpResponse;
+        import org.apache.http.client.HttpClient;
+        import org.apache.http.client.methods.HttpGet;
+        import org.apache.http.impl.client.DefaultHttpClient;
+        import org.apache.http.params.BasicHttpParams;
+
+        import android.net.ConnectivityManager;
+        import android.net.NetworkInfo;
+        import android.os.AsyncTask;
+        import android.os.Bundle;
+        import android.util.Log;
+        import android.widget.EditText;
+        import android.widget.TextView;
+        import android.widget.Toast;
+        import android.app.Activity;
 
 
-public class Hlavni_aktivita extends AppCompatActivity {
+public class Hlavni_aktivita extends Activity {
+
+    EditText etResponse;
+    TextView tvIsConnected;
     static String sURL = "http://portal.chmi.cz/files/portal/docs/uoco/web_generator/aqindex_cze.json";
-    static String sFirma = "";
-    static String sDenniMinimum = "";
-    static String sDenniMaximum = "";
-    static String sZmena = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hlavni_aktivita);
-        new NactiUdajeAsyncTask().execute();
-    }
 
-    private class NactiUdajeAsyncTask extends AsyncTask<String, String, String>
-    {
-        protected String doInBackground(String... arg0)
-        {
-            DefaultHttpClient clientHTTP =
-                    new DefaultHttpClient(new BasicHttpParams());
+        // get reference to the views
+        etResponse = (EditText) findViewById(R.id.etResponse);
+        tvIsConnected = (TextView) findViewById(R.id.tvIsConnected);
 
-            HttpPost httppost = new HttpPost(sURL);
-            httppost.setHeader("Content-type", "application/json");
-            InputStream inputStream = null;
-
-            try {
-                HttpResponse response = clientHTTP.execute(httppost);
-                HttpEntity entity = response.getEntity();
-                inputStream = entity.getContent();
-                BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"),8);
-                StringBuilder sb = new StringBuilder();
-                String radek = null;
-            }
+        // check if you are connected or not
+        if(isConnected()){
+            tvIsConnected.setBackgroundColor(0xFF00CC00);
+            tvIsConnected.setText("You are conncted");
+        }
+        else{
+            tvIsConnected.setText("You are NOT conncted");
         }
 
+        // call AsynTask to perform network operation on separate thread
+        new HttpAsyncTask().execute(sURL);
+    }
+
+    public static String GET(String url){
+        InputStream inputStream = null;
+        String result = "";
+        try {
+
+            // create HttpClient
+            HttpClient httpclient = new DefaultHttpClient();
+
+            // make GET request to the given URL
+            HttpResponse httpResponse = httpclient.execute(new HttpGet(url));
+
+            // receive response as inputStream
+            inputStream = httpResponse.getEntity().getContent();
+
+            // convert inputstream to string
+            if(inputStream != null)
+                result = convertInputStreamToString(inputStream);
+            else
+                result = "Did not work!";
+
+        } catch (Exception e) {
+            Log.d("InputStream", e.getLocalizedMessage());
+        }
+
+        return result;
+    }
+
+    private static String convertInputStreamToString(InputStream inputStream) throws IOException{
+        BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(inputStream));
+        String line = "";
+        String result = "";
+        while((line = bufferedReader.readLine()) != null)
+            result += line;
+
+        inputStream.close();
+        return result;
+
+    }
+
+    public boolean isConnected(){
+        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Activity.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected())
+            return true;
+        else
+            return false;
+    }
+    private class HttpAsyncTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+
+            return GET(urls[0]);
+        }
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(String result) {
+            Toast.makeText(getBaseContext(), "Received!", Toast.LENGTH_LONG).show();
+            etResponse.setText(result);
+        }
     }
 }
