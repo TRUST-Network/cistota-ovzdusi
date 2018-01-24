7ed1835e272323a49c2b5d86d2a031e6f095d681
prace-pc <tomas.pexa@trustnetwork.cz>
2018-01-24 14:49:26 +0100

    update

---
+++ app/src/main/java/cz/tomaspexa/android/ovzdusi/Informace.java
@@ -0,0 +1,26 @@
+package cz.tomaspexa.android.ovzdusi;
+
+/**
+ * Created by Admin on 18.1.2018.
+ */
+
+public class Informace {
+    private String code;
+    private String name;
+
+    public Informace (String code, String name ) {
+        this.code = code;
+        this.name = name;
+
+    }
+    public String getCode () {
+        return code;
+    }
+    public String getName () {
+        return name;
+    }
+    @Override
+    public String toString() {
+        return code + " " + name;
+    }
+}
